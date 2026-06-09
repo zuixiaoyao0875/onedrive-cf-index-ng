47fe77216bf4d319a3b1f3cc5c760904095d1f35
zuixiaoyao0875 <56149763+zuixiaoyao0875@users.noreply.github.com>
2026-06-10 00:54:52 +0800

    Update site.config.js
    protectedRoutes: ['/'],

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,8 @@ module.exports = {
   title: "lyc8503's OneDrive",
 
   // The folder that you are to share publicly with onedrive-cf-index-ng. Use '/' if you want to share your root folder.
-  baseDirectory: process.env.BASE_DIRECTORY || '/Transfer',
+  // baseDirectory: process.env.BASE_DIRECTORY || '/Transfer',
+  baseDirectory: process.env.BASE_DIRECTORY || '/',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
@@ -39,7 +40,8 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/Private', '/Demo/😎Another Private Folder Password 123'],
+  // protectedRoutes: ['/Private', '/Demo/😎Another Private Folder Password 123'],
+  protectedRoutes: ['/'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:me@lyc8503.net',
